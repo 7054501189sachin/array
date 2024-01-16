cf81db65d68cf20f83913a2ccfaec5e9cc374c7b
sachin mishra <sachinmis8756@gmail.com>
2024-01-16 21:12:06 +0530

    change

---
+++ .vscode/ArrayCreation.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+package .vscode;
+
+public class ArrayCreation {
+    public static void Array()
+    {
+        int marks[]=new int[100];
+        Scanner sc=new Scanne(System.in)
+    }
+    
+}
